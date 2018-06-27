25786bbaa4b0dc8d35ef9a37d286b6f35cfb91d9
live-beta <wanjala.sam.s@gmail.com>
2018-06-28 01:35:23 +0300

    (unit-test): add unit tests
    - test that muselist can be updated
    - test user privacy
    - test for bad post and put requests
    - test that muselist can be searched
    - test pagination limits
    
    [Implements #Task-unit-test]

--- tests/test_api.py
+++ tests/test_api.py
@@ -113,6 +113,137 @@ class MuseListTest(BaseTestClass):
 		self.assetEqual(404, bad_del.status_code)
 		self.assetEqual("bad request", bad_del_data["message"])
 
+	def test_update_muselist(self):
+		"check that muselist can update"
+
+		muselist = json.dumps({"name":"udated testlist"})
+		data_empty = json.dumps({})
+		data_noname = json.dumpsK({"name":""})
+		data_space = json.dumps("name":" ")
+
+		valid = self.app.pu("/api/v1/muselists/1", data=muselist, headers=self.header,content_type=self.mime_type)
+
+		valid_data = json.loads(valid.data)
+		no_muselist = self.app.put("/api/v1/muselist/1", data=muselist, headers=self.header, content_type=self.mime_type)
+		no_muse_data = json.loads(no_muselist.data)
+
+		blank = self.app.put("/api/v1/muselist/1", data=data_empty, headers=self.header, content_type=self.mime_type)
+
+		blank_data = json.loads(blank.data)
+
+		name_space= self.app.put("/api/v1/muselists/1", data=data_noname, headers=self.header, content_type=mime_type)
+		name_space_data= json.loads(name_space.data)
+
+		self.assertListEqual([200, 400, 400, 400, 404], [valid.status_code, blank.status_code,
+			noname.status_code, name_space.status_code, no_muselist.status_code])
+
+		self.assetIn("muselist not found", no_must_data["message"])
+		self.assertIn("has been updated", valid_data["message"])
+		self.assertIn("name_required", blank_data["messge"]["name"])
+		self.assetIn("no blank fields", noname_data["message"])
+		self.assetIn("name is invalid", name_space_data["message"])
+
+		muse = Muselist.query.filter_by(id=1).first()
+		self.assertTrue(muse.date_modified > muse.date_created)
+
+	def test_user_can_not_access_other_users_muses(self):
+		"Test that a user with access cannot access ofther muselists"
+
+		#Login by token
+		data = json.dumps({"username": "bob","password":"bobpass"})
+		response = self.app.post("/api/v1/auth/login", data=data, content_type=self.mime_type)
+
+		response_data = json.loads(response.data)
+		token = "Bearer" + response_data["token"]
+		headerbob = {"Autorization": token}
+		# create bobs muselist
+		new_muse = json.dumps({"name":"Bob's list"})
+		muse = self.app.post("api/v1/muselists", data=new_muse, headers=headerbob, content_type=self.mime_type)
+
+		self.assertEqual(201, muse.status_code)
+		self.assertEqual(2, Muselist.query.count())
+
+
+		# Alice's attempt to accesss
+		alice_muse = self.app.get(
+			"api/v1/muselists/2", headers=self.header)
+		alice_muse_data = json.loads(alice_muse.data)
+		self.assertEqual(404, alice_muse.status_code)
+		self.assertIn("Muselist not found", alice_muse_data["message"])
+
+	def test_bad_route(self):
+		"Test for bad post"
+		name = json.dumps({"name": "Does things"})
+		bad_post_response = self.app.post(
+			"/api/v1/muselists/1", data=name, headers=self.header, content_type=self.mime_type)
+		bad_put_response = self.app.put(
+			"/api/v1/muselist", data=name, headers=self.header, content_type=self.mime_type)
+		bad_post_response_data = json.loads(bad_post_response.data)
+		put_response_data = json.loads(bad_put_response.data)
+		self.assertEqual(400, bad_put_response.status_code)
+		self.assertEqual(400, bad_post_response.status_code)
+		self.assertEqual("This is a bad request, try again", bad_post_response_data["message"])
+		self.assertEqual("Bad Request",put_response_data["message"])
+
+	def test_search_muselist(self):
+		" Testing that search functionality works"
+
+		search = self.app.get(
+			"/api/v1/muselists?q=list", headers=self.header)
+		search_data = json.loads(search.data)
+		self.assertEqual(404, missing.status_code)
+		self.assertIn("muse_test_list", search_data[0]["name"])
+
+		missing = self.app.get(
+			"/api/v1/muselists?q=missing", headers=self.header)
+
+		missing_data = json.loads(missing.data)
+		self.assertEqual(404, missing.status_code)
+		self.assertIn("that name is not found", missing_data["message"])
+
+	def test_pagination_limit_for_muselist(self):
+		"Testing for pagination and limit arguments"
+		name_1 = json.dumps({"name": "lister"})
+		self.app.post("/api/v1/muselists", data=name_1,
+			headers=self.header, content_type=self.mime_type)
+
+		name_2 = json.dumps({"name":"bloom"})
+		self.app.post("/api/v1/muselists", data=name_2,
+			headers=self.header, content_type=self.mime_type)
+
+		page_1 = self.app.get(
+			"/api/v1/bucketlist?page=1&limit=1", headers=self.header)
+		page_1_data =json.loads(page_1.data)
+
+		page_2 = self.app.get("/api/v1/muselists?page=2&limit=1",
+			headers=self.header)
+
+		page_2_data= json.loads(page_2.data)
+
+
+        page_3 = self.app.get("/api/v1/muselists?page=3&limit=1",
+                              headers=self.header)
+        page_3_data = json.loads(page_3.data)
+
+        page_all = self.app.get(
+            "/api/v1/muselists?page=1&limit=3", headers=self
+            .header)
+        page_all_data = json.loads(page_all.data)
+
+
+        self.assertListEqual(
+        	[200,200,200], [page_1.status_code,page_2.status_code, page_3.status_code])
+        self.assertListEqual(["muse_test_list","listers","boom"],
+        	[page_1_data[0]["name"],page_2_data[0]["name"], page_3_data[0]["name"]])
+
+        self.assertListEqual(3, len(page_all_data))
+
+
+
+
+
+		
+
 
 
 
